02b82eec0fd098d9e1d9b38e27df68ec56c6623f
santhosh-recipto <santhoshkumar@recipto.in>
2025-01-08 18:49:48 +0530

    Update OurApproach.jsx

--- src/components/OurApproach.jsx
+++ src/components/OurApproach.jsx
@@ -4,7 +4,7 @@ import HeadingText from "./HeadingText";
 function Card({ imageSrc, title, description }) {
   return (
     <div className="border shadow-md text-center rounded-2xl p-6">
-      <img src={imageSrc} alt={title} className="mx-auto mb-4 w-12 h-12" />
+      <img src={imageSrc} alt={title} className="mx-auto mb-4 w-14 h-14" />
       <h3 className="text-lg font-bold mb-2">{title}</h3>
       <p className="text-sm text-gray-600">{description}</p>
     </div>
@@ -14,19 +14,19 @@ function Card({ imageSrc, title, description }) {
 export default function OurApproach() {
   const cardData = [
     {
-      imageSrc: "/path/to/icon1.png",
+      imageSrc: "/People.png",
       title: "PEOPLE",
       description:
         "A vibrant team of energetic, dynamic, and agile individuals driven by innovation and a passion for excellence.Implement customer loyalty without upgrading your systems.",
     },
     {
-      imageSrc: "/path/to/icon2.png",
+      imageSrc: "/Process.png",
       title: "PROCESS",
       description:
         "Developing tailored solutions designed to meet your unique requirements in the most effective and efficient way.",
     },
     {
-      imageSrc: "/path/to/icon3.png",
+      imageSrc: "/Tech.png",
       title: "TECHNOLOGY",
       description:
         "Adapting modern technology and intelligent methods to craft personalized solutions that address your specific needs and drive impactful results.",
